938ec97d3f3506827d2a74d4814cd475a64f90ba
smithrobertg <smithrobertg@hotmail.com>
2017-09-10 21:40:52 -0400

    Change does-not-qualify to not-eligible warning

---
+++ resources/views/survey/not-eligible.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.survey')
+
+@section('title', 'Thank you')
+
+@section('content')
+  <div class="jumbotron">
+        <p class="display-4">
+          Thank you for your time.  At this time you are not eligible to complete
+          this survey.  If you have any questions, please contact the head
+          researcher of the study, Julianne Siegfriedt at
+          <a href="mailto:julianne.siegfriedt@umb.edu">Julianne.Siegfriedt@umb.edu</a>
+        </p>
+  </div>
+@endsection
+
+@section('progress')
+  <div class="progress">
+    <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+      Survey Halted - Not Eligible
+    </div>
+  </div>
+@endsection
