dc94ea3da7fceb2b94fa6d224a4bc78140896a10
Robert Smith <smithrobertg@hotmail.com>
2018-01-10 13:46:50 -0500

    Add range progress bar for timeline event

--- resources/views/partials/timeline-event.blade.php
+++ resources/views/partials/timeline-event.blade.php
@@ -3,6 +3,8 @@
   {{ $timelineEvent->life_event->event }}
   ({{ $timelineEvent->timeframe }}: {{ $timelineEvent->age }} {{ $timelineEvent->range_from }} {{ $timelineEvent->range_to }})
 </h6>
+
+<em>Age:</em> {{ $timelineEvent->age }}
 <div class="progress">
   <!--
   <div class="progress-bar" role="progressbar" style="width: 30%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -17,4 +19,14 @@
   </div>
   <div class="progress-bar" role="progressbar" style="width: {{ 95 - $timelineEvent->age }}%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
 </div>
+
+<em>Range:</em> {{ $timelineEvent->range_from }} {{ $timelineEvent->range_to }}
+<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: {{ $timelineEvent->range_from }}%;" aria-valuenow="{{ $timelineEvent->range_from }}" aria-valuemin="0" aria-valuemax="100"></div>
+  <div class="progress-bar bg-success" role="progressbar" style="width: {{ $timelineEvent->range_to - $timelineEvent->range_from }};" aria-valuenow="{{ $timelineEvent->range_to }}" aria-valuemin="0" aria-valuemax="100">
+    <!--{{ $timelineEvent->timeframe }}: -->{{ $timelineEvent->range_from }} to {{ $timelineEvent->range_to }}
+  </div>
+  <div class="progress-bar" role="progressbar" style="width: {{ 100 - ($timelineEvent->range_to-$timelineEvent->range_from) - $timelineEvent->range_from }}%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
 <hr />
