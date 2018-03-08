afc191352bc3e115ac92ff3f3c8509f43adb9426
Bob Smith <smithrobertg@hotmail.com>
2018-03-07 22:18:25 -0500

    Include previously entered timeline events on current timeline entry

--- resources/views/survey/services-timeline.blade.php
+++ resources/views/survey/services-timeline.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.survey')
+@extends('layouts.survey-timeline')
 
 @section('title', 'Services - Followup')
 
 @section('content')
 
 <h2 class="bg-secondary text-white text-center">SERVICES TIMELINE</h2>
-<h5 class="bg-primary text-white p-3">For each of these questions, please enter AGE or RANGE the event(s) occured.  These events will be populated in timeline.</h3>
+@include('partials.timeline-header')
 
 <form method="post" action="{{ route('survey.services-timeline') }}">
     {{ csrf_field() }}
@@ -15,7 +15,7 @@
 	</div>
 
 	<div class="row">
-		<button type="submit" class="btn btn-primary btn-lg mx-auto my-4">Save &amp; Continue to Timeline &rarr;</button>
+		<button type="submit" class="btn btn-danger btn-lg mx-auto my-4">Save &amp; Continue &rarr;</button>
 	</div>
 </form>
 @endsection
